82b092e995972d59952d4365c4235ff174091076
Gabriel Alves <gabsantosalves8@gmail.com>
2024-07-27 10:33:22 -0300

    Desafio Conta Digital

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente gabriel = new Cliente();
+		gabriel.setNome("Gabriel");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(gabriel);
+		Conta poupanca = new ContaPoupanca(gabriel);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc.depositar(10000);
+		cc.transferir(10000, poupanca);
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
